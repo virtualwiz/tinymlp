e7f2a1722314af4ce8f6da89a645250e500b5f42
Matthew Du <matthewdu@protonmail.com>
2019-03-06 23:49:41 +0000

    fixed printf

--- demo.c
+++ demo.c
@@ -4,15 +4,15 @@
 int main(){
   int i;
 
-  neurone_input[0] = HIGH;
-  neurone_input[1] = LOW;
-  neurone_input[2] = LOW;
-  neurone_input[3] = LOW;
+  neurone_input[0] = 1;
+  neurone_input[1] = 0;
+  neurone_input[2] = 0;
+  neurone_input[3] = 0;
 
   MLP_Weights_Init();
   MLP_Evaluate();
 
-  for(i = 0; i <= NUM_NEURONES_OUTPUT; i++){
+  for(i = 0; i < NUM_NEURONES_OUTPUT; i++){
     printf("%lf\t", neurone_output[i]);
   }
 
