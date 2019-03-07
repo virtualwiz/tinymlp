b82bc5d64f9ad0526126ab2625128cbde4c59bbf
Matthew Du <matthewdu@protonmail.com>
2019-03-07 00:15:13 +0000

    simplified forward propagation

--- mlp.c
+++ mlp.c
@@ -1,7 +1,7 @@
 #include "mlp.h"
 
 double sigmoid(double x){
-  return 1 / (1 + (exp((double) -x)));
+  return 1 / (1 + (exp(-x)));
 }
 
 double dsigmoid(double x){
@@ -20,37 +20,20 @@ void MLP_Weights_Init(){
 
 void MLP_Evaluate(){
   int i_input, i_hidden, i_output;
-  double sum_hidden[NUM_NEURONES_HIDDEN], sum_output[NUM_NEURONES_OUTPUT];
+  double sum_hidden[NUM_NEURONES_HIDDEN] = {0}, sum_output[NUM_NEURONES_OUTPUT] = {0};
 
-  /* Initialise Sums */
-  for(i_hidden = 0; i_hidden < NUM_NEURONES_HIDDEN; i_hidden++){
-    sum_hidden[i_hidden] = 0;
-  }
-  for(i_output = 0; i_output < NUM_NEURONES_OUTPUT; i_output++){
-    sum_output[i_output] = 0;
-  }
-
-  /* Evaluate sums to hidden layer */
+  /* Evaluate sums, apply activation function and write to neurones */
   for(i_hidden = 0; i_hidden < NUM_NEURONES_HIDDEN; i_hidden++){
     for(i_input = 0; i_input < NUM_NEURONES_INPUT; i_input++){
       sum_hidden[i_hidden] += neurone_input[i_input] * weight_i_h[i_input][i_hidden];
     }
-  }
-
-  /* Apply activation function and write to hidden layer neurones */
-  for(i_hidden = 0; i_hidden < NUM_NEURONES_HIDDEN; i_hidden++){
     neurone_hidden[i_hidden] = sigmoid(sum_hidden[i_hidden]);
   }
 
-  /* Evaluate sums to output layer */
   for(i_output = 0; i_output < NUM_NEURONES_OUTPUT; i_output++){
     for(i_hidden = 0; i_hidden < NUM_NEURONES_HIDDEN; i_hidden++){
       sum_output[i_output] += neurone_hidden[i_hidden] * weight_h_o[i_hidden][i_output];
     }
-  }
-
-  /* Apply activation function and write to output layer neurones */
-  for(i_output = 0; i_output < NUM_NEURONES_OUTPUT; i_output++){
     neurone_output[i_output] = sigmoid(sum_output[i_output]);
   }
 }
