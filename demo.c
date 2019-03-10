d72e835e14419025bd956df1221e82b99577fea9
Matthew Du <matthewdu@protonmail.com>
2019-03-10 22:26:43 +0000

    added epoch limit mode

--- demo.c
+++ demo.c
@@ -2,12 +2,14 @@
 #include "stdlib.h"
 #include "mlp.h"
 
-#define DATA_LOG_ENABLED 1
+#define DATA_LOG_ENABLED 0
+#define EPOCH_LIMIT_MODE 0
 
 #define NUM_PATTERNS 4
 #define NUM_EPOCHES 100000
 #define LEARNING_RATE 0.01
-#define REPORT_INTERVAL 100
+#define TARGET_ERROR 0.1
+#define REPORT_INTERVAL 10000
 
 int main(){
   double pattern_set[NUM_PATTERNS * NUM_NEURONES_INPUT] = {
@@ -17,7 +19,7 @@ int main(){
     SOFT_LOW, SOFT_LOW, SOFT_LOW, SOFT_HIGH,
   };
   int i, j;
-  unsigned long int i_epoch;
+  unsigned long int i_epoch = 0;
   double mlp_err;
 
 #if DATA_LOG_ENABLED
@@ -38,8 +40,9 @@ int main(){
 #endif
 
   /* Start training */
-  for(i_epoch = 1; i_epoch <= NUM_EPOCHES; i_epoch++){
+  for(;;){
     MLP_Train(NUM_PATTERNS, LEARNING_RATE, pattern_set, pattern_set);
+    i_epoch += 1;
     mlp_err = MLP_ErrorAvg(NUM_PATTERNS, pattern_set, pattern_set);
     if(!(i_epoch % REPORT_INTERVAL)){
       printf("Avg error is %lf\tafter %ld epoches\n", mlp_err, i_epoch);
@@ -47,6 +50,15 @@ int main(){
       fprintf(logfile_ptr, "%ld,%lf\n", i_epoch, mlp_err);
 #endif
     }
+#if EPOCH_LIMIT_MODE
+    if(mlp_err <= TARGET_ERROR || i_epoch == NUM_EPOCHES){
+      break;
+    }
+#else
+    if(mlp_err <= TARGET_ERROR){
+      break;
+    }
+#endif
   }
 
   /* Test the neural network with patterns */
